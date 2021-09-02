cf7ffbbcfff59fccebaf794431f108985b4c118b
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-09-03 00:01:26 +0100

    feat: update migrations

---
+++ src/database/migrations/1630621239626-CreateForeignKeyUserAppointments.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class CreateForeignKeyUserAppointments1630621239626 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey('appointments', new TableForeignKey({
+      name: 'AppointmentProvider',
+      columnNames: ['provider_id'],
+      referencedColumnNames: ['id'],
+      referencedTableName: 'users',
+      onDelete: 'SET NULL',
+      onUpdate: 'CASCADE',
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+  }
+}
