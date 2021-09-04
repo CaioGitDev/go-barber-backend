a17e5e151f2ffc6a3453d9b9b08675765a6187e5
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2021-09-04 22:45:34 +0100

    fix: name

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import './database';
 
 const app = express();
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directoty));
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
